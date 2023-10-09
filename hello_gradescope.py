e2d9e03204ec7d2c9052bbcfc7e19f18e61106c3
daniel <danny.alarcon1997@gmail.com>
2023-10-09 18:05:41 -0400

    attempt 1

---
+++ hello_gradescope.py
@@ -0,0 +1,8 @@
+# Name: Daniel Alarcon Huesca
+# OSU Email: alarcoda@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 0
+# Due Date: 10/9/2023
+# Description: Hello World
+def gradescope_test():
+    return 42
